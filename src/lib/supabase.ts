26d31408028f15c5e86745498d38d689deb8f874
qlyra521-creator <qlyra521@gmail.com>
2026-04-26 18:12:49 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://dghhsuahtqlwvqnifzor.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_I3ECvjmxXtws7kQGkigO_Q_4OCkIUD1';
+const SUPABASE_URL = 'https://mjdgzqevfgvlnzpdggqp.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_j6Pm8GZZAKn_9qNXSp2jPQ_AhrhuRqq';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 export const MEDIA_BUCKET = 'memory-photos';
